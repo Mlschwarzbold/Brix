169e974ea6a6c6a9bfe62a105818a0fa99d0f68f
mlschwarzbold <miguell.schwarbold@gmail.com>
2025-09-12 18:47:50 -0300

    Client initialization

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
+#include "date_time_utils.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Processo cliente vazio " << std::endl;
+    // Validação dos argumentos da linha de comando
+    if(argc < 2) {
+        std::cerr << "Uso: ./cliente <porta>" << std::endl;
+        return 1;
+    }
+    // recebe e valida porta do servidor
+    int port;
+    try{
+        port = std::stoi(argv[1]);
+    } catch(const std::invalid_argument& e) {
+        std::cerr << "Porta inválida: " << argv[1] << std::endl;
+        return 1;
+    }
+    if(port < 0 || port > 65535) {
+        std::cerr << "Valores válidos: 0 e 65535" << std::endl;
+        return 1;
+    }
+    // Porta válida, iniciar cliente
+
+
+    std::cout << "Cliente iniciado na porta: " << port << std::endl;
+    std::cout << "Data atual: " << getCurrentDateString() << " " << getCurrentTimeString() << std::endl;
+
+
+    // Broadcast para descobrir IP do servidor
+
     return 0;
 }
