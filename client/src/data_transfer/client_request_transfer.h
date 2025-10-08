020bf2c15f2638a80145a4dd67324cf6edf0e4ca
mlschwarzbold <miguell.schwarbold@gmail.com>
2025-10-08 08:48:44 -0300

    Create empty data transfer files

---
+++ client/src/data_transfer/client_request_transfer.h
@@ -0,0 +1,6 @@
+#ifndef CLIENT_REQUEST_TRANSFER_H
+#define CLIENT_REQUEST_TRANSFER_H
+
+#include "packets/packets.h"
+
+#endif // CLIENT_REQUEST_TRANSFER_H
---
+++ common/src/data_transfer/socket_utils.h
@@ -0,0 +1,6 @@
+#ifndef SOCKET_UTILS_H
+#define SOCKET_UTILS_H
+
+
+
+#endif // SOCKET_UTILS_H
